8002e4e160db2b0c7ace148bd0fe82925c185401
Stanislav Georgiev <stanislav.georgiev@onthemarket.com>
2023-01-02 19:24:43 +0200

    Change home on back press

--- modules/presentation/src/main/java/com/github/sgeorgiev24/tradi/presentation/view/home/HomeScreen.kt
+++ modules/presentation/src/main/java/com/github/sgeorgiev24/tradi/presentation/view/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.github.sgeorgiev24.tradi.presentation.view.home
 
+import android.app.Activity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.sgeorgiev24.tradi.presentation.common.RootScreen
 import com.github.sgeorgiev24.tradi.presentation.common.components.util.TradiBackPressHandler
@@ -12,8 +14,13 @@ import com.github.sgeorgiev24.tradi.presentation.view.home.mvi.HomeViewModel
 fun HomeScreen() {
     val viewModel = hiltViewModel<HomeViewModel>()
     val state by viewModel.state.collectAsState()
+    val activity = LocalContext.current as Activity
 
-    TradiBackPressHandler()
+    TradiBackPressHandler(
+        onBackPressed = {
+            activity.finish()
+        }
+    )
 
     RootScreen(
         viewModel = viewModel,
